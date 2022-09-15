9a483f699a4f968273c379d3cfb08b2239ce57d4
amdsaad <saadamd@gmail.com>
2022-09-15 20:37:32 +0400

    dnamic update

--- components/TheHomePage/TheHero.vue
+++ components/TheHomePage/TheHero.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="hero-section">
-    <h1>this is the hero section</h1>
+    <h1>{{ sectionData }}</h1>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    sectionData: {
+      type: Object,
+      required: true,
+    },
+  },
+}
 </script>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <div class="container">
-      <the-hero />
+      <template v-for="section in pageData">
+        <component
+          :is="section.component_name"
+          v-if="section.component"
+          :key="section.id"
+          :section-data="section.content"
+        />
+      </template>
     </div>
   </div>
 </template>
@@ -9,5 +16,28 @@
 <script>
 export default {
   name: 'IndexPage',
+
+  data() {
+    return {
+      pageData: [
+        {
+          id: 1,
+          component: true,
+          component_name: 'the-hero',
+          content: {
+            h1: 'Hello World',
+          },
+        },
+        {
+          id: 2,
+          component: true,
+          component_name: 'TheAppFooter',
+          content: {
+            h1: 'Hello World 2',
+          },
+        },
+      ],
+    }
+  },
 }
 </script>
